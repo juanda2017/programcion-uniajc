3bfe1e56e0476e3359e4dc435d0dc1e5e70b8bb0
juanda2017 <juandcampo@estudiante.uniajc.edu.co>
2025-02-17 21:18:11 +0000

    clases3

---
+++ clases3/convertidor.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class convertidor {
+
+  
+}class Convertidor  
+
+  public static void main(String args[]) {
+    //daclaracion de las variables para el ejercicio
+    // c- >grados celsius, f -> grados farenheit
+    // gradosCelcius, gradosFarenheit
+    double C,f, gradosCelcius,gradosFarenheit;   
+
+    // pedir datos por teclado
+    Scanner leer = new Scanner(System.in);
+    System.out.println(x:"Digite los grasos Celcius");
+    C = leer. nextInt();
+    System.out.println(x:"Digite los Celcius");
+  
+    f = leer.nextInt();
+    leer.close();
+   
+    gradosCelcius= (f  - 32) *(5/9);
+    gradosFarenheit = C * (9/5) + 32;
+   //Mostrar los resultados de las formulas 
+   System.out.println("°F-> °C :"+gradosCelcius);
+   System.out.println("°C-> °F : "+gradosFarenheit);
+   leer.close();
+    
+
+
+
+
+ 
